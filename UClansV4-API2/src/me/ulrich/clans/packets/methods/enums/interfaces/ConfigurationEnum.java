211aca4a2db3411eba03a6d3b28f46475856d998
UlrichBR <unixmc@hotmail.com>
2021-12-26 08:03:45 -0300

    en_us lang

---
+++ UClansV4-API2/src/me/ulrich/clans/packets/methods/enums/interfaces/ConfigurationEnum.java
@@ -0,0 +1,10 @@
+package me.ulrich.clans.packets.methods.enums.interfaces;
+
+public interface ConfigurationEnum {
+    String getPath();
+
+    Object getDefaultValue();
+
+    String[] getComments();
+
+}
